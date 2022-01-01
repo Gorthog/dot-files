0370b2b4aa01bd375d420e30a738b036a811ee44
Amir Shiri <amir@sinapistech.com>
2022-01-02 00:15:39 +0200

    updated fish config

--- config.fish
+++ config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
-    fish_add_path /opt/homebrew/bin
-    abbr ll lsd -la
-    abbr fc code ~/.config/fish/config.fish
-    abbr cat bat -p
+    fish_add_path /home/linuxbrew/.linuxbrew/bin $HOME/.krew/bin
     starship init fish | source
+    abbr ll lsd -la
+    abbr fc micro ~/.config/fish/config.fish     
+    abbr cat bat -p
+    abbr sc micro ~/.ssh/config
 end
