9a5dc99fc921466d73a6486d2b9e55c6fabf1384
Amir Shiri <amir@sinapistech.com>
2022-11-27 15:51:07 +0200

    change order of kubernetes, user and add fish config file

--- config.fish
+++ config.fish
@@ -1,8 +1,48 @@
 if status is-interactive
-    fish_add_path /home/linuxbrew/.linuxbrew/bin $HOME/.krew/bin
+    set -gx PNPM_HOME "/home/amir/.local/share/pnpm"
+    fish_add_path /home/linuxbrew/.linuxbrew/bin $HOME/.krew/bin $HOME/.cargo/bin $HOME/.local/bin $HOME/.bun/bin $PNPM_HOME
+    set -Ux BUN_INSTALL $HOME/.bun
+    set _ZO_ECHO 1
     starship init fish | source
-    abbr ll lsd -la
-    abbr fc micro ~/.config/fish/config.fish     
+    zoxide init fish | source
     abbr cat bat -p
+    abbr fc micro ~/.config/fish/config.fish
+    abbr ll lsd -la --group-directories-first
     abbr sc micro ~/.ssh/config
+    abbr rm-ids "fd -H '.*:Zone.Identifier' -x rm"
+    abbr start explorer.exe
+    abbr maip http -pb ifconfig.me
+    abbr top btop
+    abbr ps procs
+    abbr cd z
+    abbr tree br
+    abbr grep rg
+    abbr find fd
+    abbr df duf
+    abbr du dua
+    abbr diff delta
+    abbr fd fd -H
+    
+    # I couldn't find a way to permanently set ulimit in WSL. but this works
+    ulimit -Sn 65535
+end
+
+function fish_user_key_bindings
+    # ctrl + backspace to delete previous word
+    bind \cH backward-kill-path-component
+    # ctrl + delete to to delete forward word
+    bind \[3\;5~ kill-word
+end
+
+# https://stackoverflow.com/questions/71502585/how-to-open-a-tab-pane-in-the-same-directory-in-windows-terminal-with-fish-shell/71502586#71502586
+function storePathForWindowsTerminal --on-variable PWD
+    if test -n "$WT_SESSION"
+      printf "\e]9;9;%s\e\\" (wslpath -w "$PWD")
+    end
+end
+
+function killPort
+    set pid (procs $argv[1] | rg "^ (\d+)" -or '$1')
+    kill -9 $pid
+    echo killed process $pid
 end
